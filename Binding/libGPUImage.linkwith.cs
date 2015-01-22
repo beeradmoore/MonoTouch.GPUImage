8eb082a2dfc8a0835267aa1d8d48d6c809c1b234
Brad Moore <b.moore@4pilabs.com.au>
2015-01-22 23:28:46 +1100

    Fixed binding/building issue once added into a project. Updated LinkTarget to match what is found in the .a

--- Binding/libGPUImage.linkwith.cs
+++ Binding/libGPUImage.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith("libGPUImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator, SmartLink = true, Frameworks = "CoreMedia CoreVideo OpenGLES AVFoundation QuartzCore", ForceLoad = true)]
+[assembly: LinkWith("libGPUImage.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, SmartLink = true, Frameworks = "CoreMedia CoreVideo OpenGLES AVFoundation QuartzCore", ForceLoad = true)]
